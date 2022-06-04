169ad033da1b265a3ba950897801bda6a3babd7a
nskrabets <skrabets@gmail.com>
2022-06-04 12:29:01 +0200

    3.4

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,14 +11,24 @@ public class ApplicationManager {
 
   WebDriver wd;
 
+  private String browser;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
 
+  public ApplicationManager(String browser) {
+    this.browser=browser;
+  }
+
   public void init() {
+    if (browser == BrowserType.CHROME){
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    }
+
     System.setProperty("webdriver.chrome.driver", "/Users/mikalaiskrabets/Work/chromedriver");
-    wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
     groupHelper = new GroupHelper(wd);
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,12 +3,13 @@ package ru.stqa.pft.addressbook.tests;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @Before
   public void setUp() throws Exception {
