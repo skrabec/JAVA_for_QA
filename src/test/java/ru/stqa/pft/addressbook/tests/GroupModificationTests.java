1a094a05d89b5bd7cc824b1858b512e987de4c95
nskrabets <skrabets@gmail.com>
2022-06-06 18:25:23 +0200

    4.4

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,14 +9,17 @@ public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("edit", "edit1", "edit2"));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().goToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
 
   }
 }
