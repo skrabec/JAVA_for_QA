73da7f773707f9257125eb75947956f9319ade19
nskrabets <skrabets@gmail.com>
2022-06-06 22:19:31 +0200

    4.5

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,8 +7,6 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
-    app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Arthur", "Conan", "Doyle", "[none]"), true);
     }
@@ -18,7 +15,5 @@ public class ContactModificationTests extends TestBase {
     app.getContactHelper().fillContactForm(new ContactData("debug", "edit", "edit", null), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomePage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
   }
 }
