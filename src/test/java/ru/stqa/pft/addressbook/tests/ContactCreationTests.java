52f7d64c5c1e3560754a4983b31813faf14d6b3f
nskrabets <skrabets@gmail.com>
2022-06-19 11:43:50 +0200

    6.4

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,21 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object []> validContacts () {
+    List<Object[]> list = new ArrayList<>();
+    list.add(new Object[] {new ContactData().withFirstName("first1").withLastName("first2").withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstName("first2").withLastName("first2").withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstName("first3").withLastName("first3").withGroup("[none]")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/lufc.png");
-    ContactData contact = new ContactData().withFirstName("Photo").withLastName("Poto").withPhoto(photo).withGroup("[none]");
     app.contact().create(contact, true);
     assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
