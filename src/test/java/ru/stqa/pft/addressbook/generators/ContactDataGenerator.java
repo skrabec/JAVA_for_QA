ae3d28757c73422daae322caa0f56dd3c99c6760
nskrabets <skrabets@gmail.com>
2022-06-19 16:50:14 +0200

    6.8

--- src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -45,17 +45,17 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)){
+      writer.write(json);
+    }
   }
 
   private static void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts){
-      writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getGroup()));
+    try (Writer writer = new FileWriter(file)){
+      for (ContactData contact : contacts){
+        writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getGroup()));
+      }
     }
-    writer.close();
   }
 
   private static List<ContactData> generateContacts(int count) {
