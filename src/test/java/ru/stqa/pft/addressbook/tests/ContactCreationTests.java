0115f3de2b206f1061f8490401d511cfce855c4a
nskrabets <skrabets@gmail.com>
2022-06-19 17:59:40 +0200

    6.9

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,9 +6,11 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -48,8 +50,9 @@ public class ContactCreationTests extends TestBase{
 
   @Test(dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) throws Exception {
+    app.goTo().gotoHomePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/lufc.png");
+    //File photo = new File("src/test/resources/lufc.png");
     app.contact().create(contact, true);
     assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
