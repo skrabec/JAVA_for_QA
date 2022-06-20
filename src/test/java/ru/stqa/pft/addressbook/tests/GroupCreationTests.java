557361fafb3982119d9d38614a965f347e0abe1e
nskrabets <skrabets@gmail.com>
2022-06-20 22:35:55 +0200

    6.11

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.tests;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -16,8 +22,19 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
   }
+
+  @BeforeMethod
+  public void logTestStart(Method method, Object [] parameter){
+    logger.info("Start test" + method.getName() + "with parameters" + Arrays.asList(parameter));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method method){
+    logger.info("Stop test" + method.getName());
+  }
+
 }
